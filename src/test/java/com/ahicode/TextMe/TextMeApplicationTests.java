ef21a276af178fc6f7a32dc04d51c472ef004faf
Ahi-Tech86 <ahi-tech86@proton.me>
2025-04-12 14:12:35 +0300

    feat<backend>: created gradle project and configured gradle.build

---
+++ src/test/java/com/ahicode/TextMe/TextMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahicode.TextMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
